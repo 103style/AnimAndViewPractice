fa0d36c2adc311cf44663ff537a4d03e1e69be1f
103style <wtimexiaoke@gmail.com>
2020-04-18 16:52:38 +0800

    fixed animatelayoutchanges bug

--- app/src/main/java/com/lxk/animandcustomviewdemo/layoutAnimation/AnimateLayoutChangesActivity.java
+++ app/src/main/java/com/lxk/animandcustomviewdemo/layoutAnimation/AnimateLayoutChangesActivity.java
@@ -122,19 +122,21 @@ public class AnimateLayoutChangesActivity extends BaseClickActivity {
         layoutTransition.setAnimator(LayoutTransition.CHANGE_APPEARING, changeAppearingAnim);
         layoutTransition.setAnimator(LayoutTransition.CHANGE_DISAPPEARING, changeDisappearingAnim);
 
+
+        int duration = 1000;
         //设置所有动画完成所需要的时长
         //setDuration(int transitionType, long duration) 对单个type设置时长
-        layoutTransition.setDuration(1000);
+        layoutTransition.setDuration(duration);
 
         //针对单个type设置插值器
         layoutTransition.setInterpolator(LayoutTransition.APPEARING, new AccelerateInterpolator());
 
         //针对单个type设置动画延时
-        layoutTransition.setStartDelay(LayoutTransition.DISAPPEARING, 1000);
+        layoutTransition.setStartDelay(LayoutTransition.DISAPPEARING, 0);
 
         //设置单个item间的动画间隔的。
-        layoutTransition.setStagger(LayoutTransition.CHANGE_APPEARING, 1000);
-        layoutTransition.setStagger(LayoutTransition.CHANGE_DISAPPEARING, 1000);
+        layoutTransition.setStagger(LayoutTransition.CHANGE_APPEARING, duration / 2);
+        layoutTransition.setStagger(LayoutTransition.CHANGE_DISAPPEARING, duration / 2);
 
         animateLayoutChangeLayout.setLayoutTransition(layoutTransition);
 
